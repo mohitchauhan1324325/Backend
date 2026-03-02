b73a0963e4a024bc135426c535b57f1b6dbc7fee
Mohit Chauhan <mohitchauhan45220@gmail.com>
2026-03-02 13:47:34 +0530

    github data added

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const data = {
+  "person": {
+    "name": "John Doe",
+    "age": 30,
+    "isStudent": false,
+    "skills": ["JavaScript", "Python"]
+  }
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/facebook', (req, res) => {
-    res.send('Mohit')
+  res.send('Mohit')
 })
 
 app.get('/login', (req, res) => {
-    res.send('<h1> Please login at Mohits Website </h1>')
+  res.send('<h1> Please login at Mohits Website </h1>')
+})
+
+app.get('/data', (req, res) => {
+  res.json(data)
 })
 
 app.listen(process.env.PORT, () => {
