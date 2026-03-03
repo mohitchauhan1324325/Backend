8695bbb431300e2e62115e40e6a9009ca1be6068
Mohit Chauhan <mohitchauhan45220@gmail.com>
2026-03-03 12:21:29 +0530

    github data added

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
-
-const data = {
-  "person": {
-    "name": "John Doe",
-    "age": 30,
-    "isStudent": false,
-    "skills": ["JavaScript", "Python"]
-  }
-}
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.get('/facebook', (req, res) => {
   res.send('Mohit')
-})
+});
 
 app.get('/login', (req, res) => {
   res.send('<h1> Please login at Mohits Website </h1>')
-})
+});
 
-app.get('/data', (req, res) => {
-  res.json(data)
-})
+app.get('/api/jokes', (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: 'joke 1',
+      content: 'This is a joke'
+    },
+    {
+      id: 2,
+      title: 'joke 2',
+      content: 'This is a joke 2'
+    },
+    {
+      id: 3,
+      title: 'joke 3',
+      content: 'This is a joke 3'
+    }
+  ];
+  res.send(jokes);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
